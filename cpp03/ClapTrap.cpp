b2ca89d552b248b8ae33d9265fe407e19f22af09
Salaheddine Badri <badrisalah47@gmail.com>
2025-11-24 23:09:53 +0100

    fffff

--- cpp03/ClapTrap.cpp
+++ cpp03/ClapTrap.cpp
@@ -1,6 +1,20 @@
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap() : _name("default"), _hitpount(10),_energyPoints(10), _attackdamage(0)
+ClapTrap::ClapTrap() :
+_name("default"), _hitpoint(10),_energypoints(10), _attackdamage(0)
 {
 	std::cout<<"ClapTrap"<<_name<<"constructed (default)"<<std::endl;
 }
+
+ClapTrap::ClapTrap(const std::string &name) :
+_name(name), _hitpoint(10),_energypoint(10),_attackdamage(0)
+{
+	std::cout<<"ClapTrap"<<_name<<"constructed"<<std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &ClapTrap)
+{
+	*this=other;
+	std::cout<<"ClapTrap"<<_name<<"copy-constructed"<<std::endl;
+}
+
